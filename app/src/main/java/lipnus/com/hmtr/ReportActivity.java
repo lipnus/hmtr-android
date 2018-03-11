385da561f614d80a90bb9d9b3ae2c2e579448f5c
lipnus <sunpil13@korea.ac.kr>
2018-03-11 14:17:18 +0900

    보고서

--- app/src/main/java/lipnus/com/hmtr/ReportActivity.java
+++ app/src/main/java/lipnus/com/hmtr/ReportActivity.java
@@ -1,23 +1,27 @@
 package lipnus.com.hmtr;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.HorizontalBarChart;
-import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.RadarData;
+import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.data.RadarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IRadarDataSet;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,8 +35,8 @@ public class ReportActivity extends AppCompatActivity {
     ImageView titleIv;
 
 
-    @BindView(R.id.report_line_chart)
-    LineChart lineChart;
+    @BindView(R.id.report_radar_chart)
+    RadarChart radarChart;
 
     @BindView(R.id.report_bar_chart)
     HorizontalBarChart barChart;
@@ -50,50 +54,137 @@ public class ReportActivity extends AppCompatActivity {
         this.overridePendingTransition(R.anim.fadein, R.anim.fadeout);
 
         setTitle();
-        setChart();
+        setRadarChart();
+        setBarChar();
     }
 
-    public void setChart(){
+    public void setRadarChart(){
 
-        List<Entry> entries = new ArrayList<Entry>();
+        radarChart.setBackgroundColor(Color.rgb(60, 65, 82));
+        radarChart.getDescription().setEnabled(false);
 
-        entries.add( new Entry(1,20) );
-        entries.add( new Entry(2,30) );
-        entries.add( new Entry(3,10) );
-        entries.add( new Entry(4,50) );
+        radarChart.setWebLineWidth(1f);
+        radarChart.setWebColor(Color.LTGRAY);
+        radarChart.setWebLineWidthInner(1f);
+        radarChart.setWebColorInner(Color.LTGRAY);
+        radarChart.setWebAlpha(100);
+        setData();
 
-        LineDataSet lineDataSet = new LineDataSet(entries, "ㅋㅋㅋ");
-        lineDataSet.setColor( Color.parseColor("#604a7b") );
+        radarChart.animateXY(
+                1400, 1400,
+                Easing.EasingOption.EaseInOutQuad,
+                Easing.EasingOption.EaseInOutQuad);
 
-        LineData lineData = new LineData(lineDataSet);
-        lineChart.setData( lineData );
-        lineChart.invalidate();
+        XAxis xAxis = radarChart.getXAxis();
+        xAxis.setTextSize(9f);
+        xAxis.setYOffset(0f);
+        xAxis.setXOffset(0f);
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+
+            private String[] mActivities = new String[]{"채움형", "세움형", "키움형", "돋움형"};
+
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return mActivities[(int) value % mActivities.length];
+            }
+        });
+        xAxis.setTextColor(Color.WHITE);
 
 
 
 
+        YAxis yAxis = radarChart.getYAxis();
+        yAxis.setLabelCount(4, false);
+        yAxis.setTextSize(10f);
+        yAxis.setAxisMinimum(0f);
+        yAxis.setAxisMaximum(100f);
+        yAxis.setDrawLabels(false);
+
+        radarChart.getDescription().setEnabled(false);
+        radarChart.setWebLineWidth(1f);
+        radarChart.setWebColor(Color.LTGRAY);
+        radarChart.setWebLineWidthInner(1f);
+        radarChart.setWebColorInner(Color.LTGRAY);
+        radarChart.setWebAlpha(100);
+
+
+        radarChart.invalidate();
+    }
+
+    public void setData() {
+        int count = 4;
+
+        ArrayList<Integer> result;
+        Intent get_intent = getIntent();
+        result = (ArrayList<Integer>) get_intent.getSerializableExtra("result");
+
+        ArrayList<RadarEntry> entries1 = new ArrayList<RadarEntry>();
+
+
+
+        // NOTE: The order of the entries when being added to the entries array determines their position around the center of
+        // the chart.
+        for (int i = 0; i < count; i++) {
+            entries1.add(new RadarEntry(i*20+10));
+        }
+
+        RadarDataSet set1 = new RadarDataSet(entries1, "DAESUNG ACADEMY");
+        set1.setColor(Color.rgb(103, 110, 129));
+        set1.setFillColor(Color.rgb(103, 110, 129));
+        set1.setDrawFilled(true);
+        set1.setFillAlpha(180);
+        set1.setLineWidth(2f);
+        set1.setDrawHighlightCircleEnabled(true);
+        set1.setDrawHighlightIndicators(false);
+
+        ArrayList<IRadarDataSet> sets = new ArrayList<IRadarDataSet>();
+        sets.add(set1);
+
+        RadarData data = new RadarData(sets);
+        data.setValueTextSize(8f);
+        data.setDrawValues(false);
+        data.setValueTextColor(Color.WHITE);
+
+        radarChart.setData(data);
+        radarChart.invalidate();
+    }
+
+
+
+
+    public void setBarChar(){
         List<BarEntry> entries2 = new ArrayList<>();
-        entries2.add(new BarEntry(0, 30f));
-        entries2.add(new BarEntry(1f, 80f));
+        entries2.add(new BarEntry(0, 10f));
+        entries2.add(new BarEntry(1f, 20f));
         entries2.add(new BarEntry(2f, 60f));
         entries2.add(new BarEntry(3f, 30f));
 
 
-        BarDataSet set = new BarDataSet(entries2, "행동유형척도");
-        BarData data = new BarData(set);
+        BarDataSet dataSet = new BarDataSet(entries2, "행동유형척도");
+        dataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+        dataSet.setColor( Color.parseColor("#604a7b"));
 
+        BarData data = new BarData(dataSet);
         data.setBarWidth(0.5f); // set custom bar width
 
 
         barChart.setData(data);
-        barChart.setVisibleYRange(0,100f, YAxis.AxisDependency.RIGHT);
-        barChart.set
+        barChart.setDoubleTapToZoomEnabled(false);
+
+        //기준열
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setEnabled(false);
+
+        //위쪽
+        YAxis leftAxis = barChart.getAxisLeft();
+        leftAxis.setEnabled(false);
+
+        //아래쪽
+        YAxis rightAxis = barChart.getAxisRight();
+        rightAxis.setAxisMinimum(0);
+        rightAxis.setAxisMaximum(100);
 
         barChart.invalidate(); // refresh
-
-
-
-
     }
 
     public void setTitle(){
